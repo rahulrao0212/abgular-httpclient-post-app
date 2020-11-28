69ab3e5e7c883e55d4d8bfcb9a46859068d5a44b
Rahul Rao <RAOR378647@exactsoftware.com>
2020-11-28 20:55:13 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Person } from './model/person';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'httpclient-post-app';
+  people: Person[] | undefined;
+  person = new Person();
+
+  constructor(private apiService: ApiService) {
+
+  } 
+
+  ngOnInit() {
+    this.refreshPeople()
+  }
+
+  refreshPeople() {
+    this.apiService.getPeople()
+      .subscribe(data => {
+        console.log(data)
+        this.people = data;
+      })
+  }
+
+  addPerson() {
+    this.apiService.addPerson(this.person)
+      .subscribe(data => {
+        console.log(data)
+        this.refreshPeople();
+      })
+  }
 }
